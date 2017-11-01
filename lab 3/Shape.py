10c33b6d6981e74cfc1a0633ff5517c5c1544c69
Yonatan <yonatanfatal2411@gmail.com>
2017-11-01 19:23:15 +0200

    nourishes

---
+++ lab 3/Shape.py
@@ -0,0 +1,5 @@
+import turtle
+
+turtle.register_shape("ArrowLike", ((0,0), (0,50), (50, 50), (50,0), (20,-25)))
+turtle.shape("ArrowLike")
+turtle.mainloop()
---
+++ lab 3/Stars.py
@@ -0,0 +1,8 @@
+import turtle
+
+for i in range(5):
+	turtle.forward(150)
+	turtle.right(144)
+
+
+turtle.mainloop()
