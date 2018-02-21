d45f56f4657258c72e16f3ac8b4eb87939926fe7
Yonatan <yonatan@gmail.com>
2018-02-21 13:07:47 +0200

    Hey!

--- Yonatan'sPartBBTAN.py
+++ Yonatan'sPartBBTAN.py
@@ -4,11 +4,20 @@ import random
 import math
 colormode(255)
 
+tracer(0)
+ht()
+
+SCREEN_WIDTH = getcanvas().winfo_width()/2
+SCREEN_HEIGHT = getcanvas().winfo_height()/2
+
+
 class Ball(Turtle):
-	def __init__(self, x,y,dx,dy):
+	def __init__(self,dx,dy):
 		Turtle.__init__(self)
 		self.pu()
-		self.goto(x,y)
+		self.ht()
+		self.goto(0, -SCREEN_HEIGHT + 50)
+		self.st()
 		self.dx = dx
 		self.dy = dy
 		self.shape("circle")
@@ -29,37 +38,105 @@ class Ball(Turtle):
 		bottom_ball = new_y - r
 		upper_ball_side = new_y + r
 		self.goto(new_x, new_y)
-		if bottom_ball < -screen_hight or upper_ball_side > screen_hight:
+		if bottom_ball < -screen_hight or upper_ball_side > (screen_hight - 3*r):
 			self.dy *= -1
-		if left_side_ball < -screen_width or right_side_ball > screen_width:
+		elif left_side_ball < (-screen_width + 3*r) or right_side_ball > (screen_width - 3*r):
 			self.dx *= -1
 
-SCREEN_WIDTH = getcanvas().winfo_width()/2
-SCREEN_HEIGHT = getcanvas().winfo_height()/2
+class Square(Turtle):
+	def __init__(self, health, serial_number):
+		Turtle.__init__(self)
+		self.health = health
+		self.serial_number = serial_number
+		self.color("Pink")
+		global r
+		r = 10
+		global SCREEN_WIDTH
+		square_size = ((SCREEN_WIDTH*2 - 6*r)/6)
+		self.begin_poly()
+		self.pu()
+		
+		for i in range(4):
+			fd(square_size - 10)
+			left(90)
+		self.end_poly()
+		p = self.get_poly()
+		register_shape("poly",p)
+		self.pd()
+		self.shape("poly")
+	def life(health):
+		pu()
+		fd(-square_size*0.5 - 5)
+		left(90)
+		fd(square_size*0.5 - 5)
+		write(1)
+		fd(-square_size*0.5 + 5)
+		right(90)
+		fd(square_size*0.5 + 5)
+
+
+SQUARE = []		
+def create(r, SCREEN_WIDTH, SCREEN_HEIGHT):
+	global square_size
+	square_size = ((SCREEN_WIDTH*2 - 6*r)/6)
+	st()
+	
+	b = 0
+	for i in range(4):
+		pu()
+		goto(-SCREEN_WIDTH + 2*r, SCREEN_HEIGHT - (i+1)*square_size)
+		goto(-SCREEN_WIDTH + 3*r, SCREEN_HEIGHT - (i+1)*square_size)
+		for square in range(6):
+			b+=1
+			pd()
+			square = Square(3,b)
+			fd(square_size)
+			SQUARE.append(square)
+			square.life()
+			print("new square")
+
+create(10, SCREEN_WIDTH, SCREEN_HEIGHT)
+
+
+
+
+
 
 Balls = []
 
 def create_ball(event):
+	global NUMBER_OF_BALLS
+	global Balls
+	NUMBER_OF_BALLS = 1
 	for i in range(NUMBER_OF_BALLS):
-		print("hello")
+		# print("hello")
 		x = 0
 		y = -SCREEN_HEIGHT + 50
-		# while dx == 0:
-		# 	dx = random.uniform(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
-		# while dy == 0:
-		# 	dy = random.uniform(MINIMUM_BALL_DY , MAXIMUM_BALL_DY)
-		m = (event.y - y) / (event.x - x)
-		if event.x <= 0:
-			dx = -1
-		if event.x > 0:
-			dx = +1
-		dy = m*dx
-		new_ball = Ball(x, y, dx, dy, r)
-		print(new_ball)
-		if new_ball is None:
-			print("Ball is none")
-		BALLS.append(new_ball)
-Ball1 = Ball(0,-50,1,1) 
+		dx = event.x - SCREEN_WIDTH - x
+		dy = SCREEN_HEIGHT - event.y - y
+		new_ball = Ball(dx/100, dy/100)
+		Balls.append(new_ball)
+		new_ball.move(SCREEN_WIDTH,SCREEN_HEIGHT)
+		print("new ball")
+
+		
+
+getcanvas().bind("<Button-1>", create_ball)
+
 while True:
-	Ball1.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+	print(len(Balls))
+	for ball in Balls:
+		# time.sleep(0.0077)
+		if ball.ycor() >= (-SCREEN_HEIGHT + 50):
+			ball.move(SCREEN_WIDTH,SCREEN_HEIGHT)
+		else:
+			ball.goto(0,-SCREEN_HEIGHT + 49)
+	getscreen().update()
+# Ball1 = Ball(1,1) 
+# while True:
+# 	Ball1.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+# create_ball(event)
+# create_ball(event)
+
 mainloop()
+
